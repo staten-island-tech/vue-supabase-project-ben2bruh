6c58d66329223bac030f3392ae4b814e9330dd3e
RyanO232010 <ryano92@nycstudents.net>
2026-05-04 12:13:56 -0400

    1

--- nuxt-app/nuxt.config.ts
+++ nuxt-app/nuxt.config.ts
@@ -2,5 +2,5 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@pinia/nuxt', '@nuxtjs/tailwindcss']
+  modules: ['@pinia/nuxt', '@nuxtjs/tailwindcss', '@nuxt/fonts']
 })
